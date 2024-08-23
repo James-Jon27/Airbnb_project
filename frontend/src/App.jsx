e783002500a7c24614a6c582bcf4533e5fcea066
James-Jon27 <jamescjones818@gmail.com>
2024-08-23 16:18:08 -0600

    NavBar with Toggle Menu

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
 import LoginFormPage from './components/LoginFormPage';
 import SignUpFormPage from './components/SignUpFormPage';
 import * as sessionActions from './store/session';
+import Navigation from './components/Navigation';
 
 function Layout() {
   const dispatch = useDispatch();
@@ -17,6 +18,7 @@ function Layout() {
 
   return (
     <>
+    <Navigation isLoaded={isLoaded}/>
       {isLoaded && <Outlet />}
     </>
   );
