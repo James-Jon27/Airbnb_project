d4b8efaaea4dd7cbe41f834ac2a7a06e69c86653
James-Jon27 <jamescjones818@gmail.com>
2024-08-25 02:15:39 -0600

    render all spots

--- frontend/src/components/Spots/Spots.jsx
+++ frontend/src/components/Spots/Spots.jsx
@@ -4,29 +4,33 @@ import { MdStars } from "react-icons/md";
 export default function Spots() {
 	const allSpots = useSelector((state) => state.spots);
 	const spotsIterable = Object.values(allSpots);
-    const spots = []
+	const spots = [];
 	for (const spot of spotsIterable) {
-        spots.push(spot)
-    }
+		spots.push(spot);
+	}
 
 	return (
 		<div>
-			<ul>{
-                spots.map(({id, city, state, avgStarRating, price, previewImage, name}) => {
-                    return (
-                        <div key={id}>
-                            <img src={previewImage} alt={name} style={{width: "25rem"}}/>
-                            <span>
-                                <p>{city}, {state}</p>
-                                <span>
-                                <MdStars /><p>{avgStarRating}</p>
-                                </span>
-                            </span>
-                            <span>{price}/night</span>
-                        </div>
-                    )
-                })
-                }</ul>
+			<ul>
+				{spots.map(({ id, city, state, avgStarRating, price, previewImage, name }) => {
+                    console.log(id, city, state, avgStarRating, previewImage, price, name)
+					return (
+                            <div key={id} className="tile">
+								<img src={previewImage} alt={name} style={{ width: "25rem" }} />
+								<span>
+									<p>
+										{city}, {state}
+									</p>
+									<span>
+										<MdStars />
+										<p>{avgStarRating}</p>
+									</span>
+								</span>
+								<span>{price}/night</span>
+                            </div>
+					);
+				})}
+			</ul>
 		</div>
 	);
 }
